a3c4642db6761ee10a0424c15ca53574ae72ecad
Sander <sander.paasalu@khk.ee>
2021-01-19 11:49:38 +0200

    jep

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter, Switch, Route, Link, useParams } from "react-router-dom";
+import NewRecipe from './components/NewRecipe';
+import Recipe from "./components/Recipe";
+import RecipeList from './components/RecipeList';
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);  
+  function getRecipes() {
+    fetch("data/data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+    .then(result => {
+    return result.json();
+    })
+    .then(data => {
+    console.log(data);
+    setRecipes(data)
+    })
+  }
+
+  function addRecipe(newRecipe) {
+    setRecipes(recipes.concat([newRecipe]));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <h1 className='flex-main' className='header-main'>Retseptiraamat</h1>
+          <Link className='flex-main' className='link-main' to={'/new'}>Lisa uus retsept</Link>
+          <RecipeList recipes={recipes} />
+        </Route>
+        <Route path="/recipes/:id">
+          <Recipe recipes={recipes} />
+        </Route>
+        <Route path='/new'>
+          <NewRecipe addRecipe={addRecipe} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
